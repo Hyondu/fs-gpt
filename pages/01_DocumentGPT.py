171da412c6573665670d0fcadc14c3a2d4bc2378
Hyondu <147678410+Hyondu@users.noreply.github.com>
2024-04-22 07:13:17 +0900

    [DocumentGPT] testing chain memory

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -11,6 +11,11 @@ from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain.memory import ConversationSummaryBufferMemory
+from langchain.globals import set_debug, set_verbose
+
+
+set_debug(False)
+set_verbose(False)
 
 
 st.set_page_config(
@@ -18,12 +23,13 @@ st.set_page_config(
     page_icon="📑"
 )
 
+
 with st.sidebar:
-    model = st.selectbox("Choose LLM model",
-                         ("mistral:latest", "llama2:latest", "llama2:13b"))
-    temperature = st.slider("Temperature", 0.1, 1.0)
-    file = st.file_uploader("upload a .txt .pdf or .docx file", type=[
-                            "txt", "pdf", "docx"])
+  model = st.selectbox("Choose LLM model",
+                       ("mistral:latest", "llama2:latest", "llama2:13b"))
+  temperature = st.slider("Temperature", 0.1, 1.0)
+  file = st.file_uploader("upload a .txt .pdf or .docx file", type=[
+                          "txt", "pdf", "docx"])
 
 
 st.title("DocumentGPT")
@@ -38,78 +44,78 @@ Upload your file on the sidebar.
 
 
 def send_message(msg, role, save=True):
-    with st.chat_message(role):
-        st.markdown(msg)
-    if save:
-        save_massage(msg, role)
+  with st.chat_message(role):
+    st.markdown(msg)
+  if save:
+    save_massage(msg, role)
 
 
 def save_massage(msg, role):
-    st.session_state.messages.append({"message": msg, "role": role})
+  st.session_state.messages.append({"message": msg, "role": role})
 
 
 def save_memory(input, output):
-    st.session_state.memory.save_context({"input": input}, {"output": output})
+  st.session_state.memory.save_context({"input": input}, {"output": output})
 
 
 def load_memory(_):
-    return st.session_state.memory.load_memory_variables({})["history"]
+  return st.session_state.memory.load_memory_variables({})["memory"]
 
 
 def invoke_chain(chain, msg):
-    res = chain.invoke(msg)
-    save_memory(msg, res.content)
+  res = chain.invoke(msg)
+  save_memory(msg, res.content)
 
 
 def paint_history():
-    for msg in st.session_state["messages"]:
-        send_message(msg["message"], msg["role"], save=False)
+  for msg in st.session_state["messages"]:
+    send_message(msg["message"], msg["role"], save=False)
 
 
 def format_docs(documents):
-    return "\n\n".join(doc.page_content for doc in documents)
+  return "\n\n".join(doc.page_content for doc in documents)
 
 
 @st.cache_data(show_spinner="Embedding the file...")
 def embed_file(file):
-    file_content = file.read()
-    file_path = f"./.cache/files/{file.name}"
-    with open(file_path, "wb") as f:
-        f.write(file_content)
-    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
-    splitter = CharacterTextSplitter.from_tiktoken_encoder(
-        separator="\n",
-        chunk_size=600,
-        chunk_overlap=100,
-    )
-    loader = UnstructuredFileLoader(file_path)
-    docs = loader.load_and_split(text_splitter=splitter)
-    embeddings = OllamaEmbeddings(model="mistral:latest")
-    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
-        embeddings, cache_dir)
-    db = FAISS.from_documents(docs, cached_embeddings)
-    retriever = db.as_retriever()
-    return retriever
+  file_content = file.read()
+  file_path = f"./.cache/files/{file.name}"
+  with open(file_path, "wb") as f:
+    f.write(file_content)
+  cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+  splitter = CharacterTextSplitter.from_tiktoken_encoder(
+      separator="\n",
+      chunk_size=600,
+      chunk_overlap=100,
+  )
+  loader = UnstructuredFileLoader(file_path)
+  docs = loader.load_and_split(text_splitter=splitter)
+  embeddings = OllamaEmbeddings(model="mistral:latest")
+  cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+      embeddings, cache_dir)
+  db = FAISS.from_documents(docs, cached_embeddings)
+  retriever = db.as_retriever()
+  return retriever
 
 
 class ChatCallbackHandler(BaseCallbackHandler):
-    def __init__(self):
-        self.message = ""
-        self.message_box = None
+  def __init__(self):
+    self.message = ""
+    self.message_box = None
 
-    def on_llm_start(self, *args, **kwargs):
-        self.message_box = st.empty()
-        with st.sidebar:
-            st.write("llm started")
+  def on_llm_start(self, *args, **kwargs):
+    self.message_box = st.empty()
+    with st.sidebar:
+      st.write("llm started")
 
-    def on_llm_end(self, *args, **kwargs):
-        save_massage(self.message, "ai")
-        with st.sidebar:
-            st.write("llm ended")
+  def on_llm_end(self, *args, **kwargs):
+    save_massage(self.message, "ai")
+    with st.sidebar:
+      st.write("llm ended")
 
-    def on_llm_new_token(self, token, *args, **kwargs):
-        self.message += token
-        self.message_box.markdown(self.message)
+  def on_llm_new_token(self, token, *args, **kwargs):
+    self.message += token
+    self.message_box.markdown(self.message)
 
 
 chat = ChatOllama(model=model, temperature=temperature,
@@ -129,29 +135,30 @@ prompt = ChatPromptTemplate.from_messages([
 
 
 if file:
-    retriever = embed_file(file)
-    send_message("I'm ready! Ask away!", "ai", save=False)
-    paint_history()
-    msg = st.chat_input("Ask anything about your file...")
-    if msg:
-        send_message(msg, "human")
-        # chain = {
-        #     "context": retriever | RunnableLambda(format_docs),
-        #     "question": RunnablePassthrough(),
-        # } | RunnablePassthrough.assign(
-        #     history=RunnableLambda(
-        #         st.session_state.memory.load_memory_variables
-        #     ) | itemgetter("history")
-        # ) | prompt | chat
-        chain = {
-            "context": retriever | RunnableLambda(format_docs),
-            "question": RunnablePassthrough(),
-        } | RunnablePassthrough.assign(history=load_memory) | prompt | chat
-        with st.chat_message("ai"):
-            invoke_chain(chain, msg)
+  retriever = embed_file(file)
+  send_message("I'm ready! Ask away!", "ai", save=False)
+  paint_history()
+  msg = st.chat_input("Ask anything about your file...")
+  if msg:
+    send_message(msg, "human")
+    print([k for k in st.session_state.keys()])
+    # chain = {
+    #     "context": retriever | RunnableLambda(format_docs),
+    #     "question": RunnablePassthrough(),
+    # } | RunnablePassthrough.assign(
+    #     history=RunnableLambda(
+    #         st.session_state.memory.load_memory_variables
+    #     ) | itemgetter("history")
+    # ) | prompt | chat
+    chain = {
+        "context": retriever | RunnableLambda(format_docs),
+        "question": RunnablePassthrough(),
+    } | RunnablePassthrough.assign(history=load_memory) | prompt | chat
+    with st.chat_message("ai"):
+      invoke_chain(chain, msg)
 else:
-    st.session_state["messages"] = []
-    st.session_state["memory"] = ConversationSummaryBufferMemory(
-        llm=chat, max_token_limit=2000, return_messages=True
-    )
-    print(st.session_state.keys())
+  st.session_state["messages"] = []
+  st.session_state["memory"] = ConversationSummaryBufferMemory(
+      llm=chat, max_token_limit=2000, return_messages=True
+  )
+  print([k for k in st.session_state.keys()])
